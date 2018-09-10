7c0472c1f1634459636f6d9bb597d6db60c11a94
Haruki Sakai <h.s.13415.b@gmail.com>
2018-09-10 17:01:59 +0900

    Add files via upload

---
+++ WSB2017/sidebar.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    // 向き
+    var sides = ["left", "top", "right", "bottom"];
+
+    // サイドバーの初期化
+    for (var i = 0; i < sides.length; ++i) {
+        var cSide = sides[i];
+        $(".sidebar." + cSide).sidebar({side: cSide});
+    }
+
+    // ボタンのクリックにより...
+    $(".btn[data-action]").on("click", function () {
+        var $this = $(this);
+        var action = $this.attr("data-action");
+        var side = $this.attr("data-side");
+        $(".sidebar." + side).trigger("sidebar:" + action);
+        return false;
+    });
+});
